ed72f733504a22a29d0a34098a97e5bf1217d5d3
Dmitry Lenchevski <dima.lenchevskii@gmail.com>
2020-04-12 15:07:44 +0300

    refactoring

--- permutations/permutations.js
+++ permutations/permutations.js
@@ -2,23 +2,23 @@
 
 const args            = process.argv.slice(2)
 
-const compose         = (...fns) => (x) => fns.reduce((y, f) => f(y), x)
+const compose         = (...fns)  => (x) => fns.reduce((y, f) => f(y), x)
 
-const hasDuplicates   = (arr) => arr.some((item) => arr.indexOf(item) != arr.lastIndexOf(item))
+const hasDuplicates   = (arr)     => arr.some((item) => arr.indexOf(item) != arr.lastIndexOf(item))
 
-const sortInside      = (arr) => arr.map((x) => x.split('').sort().join(''))
+const sortInside      = (arr)     => arr.map((x) => x.split('').sort().join(''))
 
-const checkAll        = (arr) => hasDuplicates(arr) ? repeatables(arr) : Array([0])
+const checkAll        = (arr)     => hasDuplicates(arr) ? repeatables(arr) : Array([0])
 
-const repeatables     = (arr)   => arr.map((x) => arr.map((elem, index) => elem === x ? index : undefined)
+const repeatables     = (arr)     => arr.map((x) => arr.map((elem, index) => elem === x ? index : undefined)
                                               .filter(z => z != undefined))
                                             .filter(list => list.length > 1)
 
-const getMaxOf2dArray = (arr) => arr.reduce((max, p) => p.length > max.length ? p : max)
+const getMaxOf2dArray = (arr)     => arr.reduce((max, p) => p.length > max.length ? p : max)
 
-const getElemsByArray = (arr) => arr.map(x => args[x])
+const getElemsByArray = (arr)     => arr.map(x => args[x])
 
-const prettyPrint     = (arr) => arr.map(x => x != undefined ? console.log(x) : undefined)
+const prettyPrint     = (arr)     => arr.map(x => x != undefined ? console.log(x) : undefined)
 
 const permutations    = compose(sortInside, checkAll, getMaxOf2dArray, getElemsByArray, prettyPrint)
 
